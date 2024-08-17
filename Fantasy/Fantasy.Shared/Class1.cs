7c9c9e486c80f002b899caa06280c34214020d2f
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-08-17 10:30:11 -0500

    Basic project creation

---
+++ Fantasy/Fantasy.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Fantasy.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Fantasy/Fantasy.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Fantasy.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
