ada03ca393d83ab740e40709b793c4f8573bd472
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-08-30 21:28:33 -0500

    Add DB Seeder

---
+++ Fantasy/Fantasy.Backend/Data/SeedDb.cs
@@ -0,0 +1,50 @@
+﻿using Fantasy.Shared.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fantasy.Backend.Data;
+
+public class SeedDb
+{
+    private readonly DataContext _context;
+
+    public SeedDb(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SeedAsync()
+    {
+        await _context.Database.EnsureCreatedAsync();
+        await CheckCountriesAsync();
+        await CheckTeamsAsync();
+    }
+
+    private async Task CheckCountriesAsync()
+    {
+        if (!_context.Countries.Any())
+        {
+            var countriesSQLScript = File.ReadAllText("Data\\Countries.sql");
+            await _context.Database.ExecuteSqlRawAsync(countriesSQLScript);
+        }
+    }
+
+    private async Task CheckTeamsAsync()
+    {
+        if (!_context.Teams.Any())
+        {
+            foreach (var country in _context.Countries)
+            {
+                _context.Teams.Add(new Team { Name = country.Name, Country = country! });
+                if (country.Name == "Colombia")
+                {
+                    _context.Teams.Add(new Team { Name = "Medellín", Country = country! });
+                    _context.Teams.Add(new Team { Name = "Nacional", Country = country! });
+                    _context.Teams.Add(new Team { Name = "Millonarios", Country = country! });
+                    _context.Teams.Add(new Team { Name = "Junior", Country = country! });
+                }
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- Fantasy/Fantasy.Backend/Program.cs
+++ Fantasy/Fantasy.Backend/Program.cs
@@ -11,11 +11,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddTransient<SeedDb>();
 
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using var scope = scopedFactory!.CreateScope();
+    var service = scope.ServiceProvider.GetService<SeedDb>();
+    service!.SeedAsync().Wait();
+}
 
 if (app.Environment.IsDevelopment())
 {
