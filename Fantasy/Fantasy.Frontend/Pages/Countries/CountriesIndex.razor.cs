27105899ed1bc6c9dfe4be8b0f54df228dae8a8d
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-09-13 20:27:39 -0500

    Countries index with MudBlazor

--- Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
+++ Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -1,80 +1,150 @@
-using CurrieTechnologies.Razor.SweetAlert2;
+using System.Net;
 using Fantasy.Frontend.Repositories;
+using Fantasy.Frontend.Shared;
 using Fantasy.Shared.Entites;
 using Fantasy.Shared.Resources;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
+using MudBlazor;
 
 namespace Fantasy.Frontend.Pages.Countries;
 
 public partial class CountriesIndex
 {
-    [Inject] private IRepository Repository { get; set; } = null!;
-    [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
-    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
-    [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
-
     private List<Country>? Countries { get; set; }
+    private MudTable<Country> table = new();
+    private readonly int[] pageSizeOptions = { 10, 25, 50, int.MaxValue };
+    private int totalRecords = 0;
+    private bool loading;
+    private const string baseUrl = "api/countries";
+    private string infoFormat = "{first_item}-{last_item} => {all_items}";
+
+    [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private IDialogService DialogService { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+
+    [Parameter, SupplyParameterFromQuery] public string Filter { get; set; } = string.Empty;
 
     protected override async Task OnInitializedAsync()
     {
-        await LoadAsync();
+        await LoadTotalRecordsAsync();
     }
 
-    private async Task LoadAsync()
+    private async Task LoadTotalRecordsAsync()
     {
-        var responseHppt = await Repository.GetAsync<List<Country>>("api/countries");
-        if (responseHppt.Error)
+        loading = true;
+        var url = $"{baseUrl}/totalRecordsPaginated";
+
+        if (!string.IsNullOrWhiteSpace(Filter))
         {
-            var message = await responseHppt.GetErrorMessageAsync();
-            await SweetAlertService.FireAsync(Localizer["Error"], message, SweetAlertIcon.Error);
+            url += $"?filter={Filter}";
+        }
+
+        var responseHttp = await Repository.GetAsync<int>(url);
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(Localizer[message!], Severity.Error);
             return;
         }
-        Countries = responseHppt.Response!;
+
+        totalRecords = responseHttp.Response;
+        loading = false;
+    }
+
+    private async Task<TableData<Country>> LoadListAsync(TableState state, CancellationToken cancellationToken)
+    {
+        int page = state.Page + 1;
+        int pageSize = state.PageSize;
+        var url = $"{baseUrl}/paginated/?page={page}&recordsnumber={pageSize}";
+
+        if (!string.IsNullOrWhiteSpace(Filter))
+        {
+            url += $"&filter={Filter}";
+        }
+
+        var responseHttp = await Repository.GetAsync<List<Country>>(url);
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(Localizer[message!], Severity.Error);
+            return new TableData<Country> { Items = [], TotalItems = 0 };
+        }
+        if (responseHttp.Response == null)
+        {
+            return new TableData<Country> { Items = [], TotalItems = 0 };
+        }
+        return new TableData<Country>
+        {
+            Items = responseHttp.Response,
+            TotalItems = totalRecords
+        };
+    }
+
+    private async Task SetFilterValue(string value)
+    {
+        Filter = value;
+        await LoadTotalRecordsAsync();
+        await table.ReloadServerData();
+    }
+
+    private async Task ShowModalAsync(int id = 0, bool isEdit = false)
+    {
+        var options = new DialogOptions() { CloseOnEscapeKey = true, CloseButton = true };
+        IDialogReference? dialog;
+        if (isEdit)
+        {
+            var parameters = new DialogParameters
+            {
+                { "Id", id }
+            };
+            dialog = DialogService.Show<CountryEdit>($"{Localizer["Edit"]} {Localizer["Country"]}", parameters, options);
+        }
+        else
+        {
+            dialog = DialogService.Show<CountryCreate>($"{Localizer["New"]} {Localizer["Country"]}", options);
+        }
+
+        var result = await dialog.Result;
+        if (result!.Canceled)
+        {
+            await LoadTotalRecordsAsync();
+            await table.ReloadServerData();
+        }
     }
 
     private async Task DeleteAsync(Country country)
     {
-        var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+        var parameters = new DialogParameters
         {
-            Title = Localizer["Confirmation"],
-            Text = string.Format(Localizer["DeleteConfirm"], Localizer["Country"], country.Name),
-            Icon = SweetAlertIcon.Question,
-            ShowCancelButton = true,
-            CancelButtonText = Localizer["Cancel"]
-        });
-
-        var confirm = string.IsNullOrEmpty(result.Value);
-
-        if (confirm)
+            { "Message", string.Format(Localizer["DeleteConfirm"], Localizer["Country"], country.Name) }
+        };
+        var options = new DialogOptions { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall, CloseOnEscapeKey = true };
+        var dialog = DialogService.Show<ConfirmDialog>(Localizer["Confirmation"], parameters, options);
+        var result = await dialog.Result;
+        if (result!.Canceled)
         {
             return;
         }
 
-        var responseHttp = await Repository.DeleteAsync($"api/countries/{country.Id}");
+        var responseHttp = await Repository.DeleteAsync($"{baseUrl}/{country.Id}");
         if (responseHttp.Error)
         {
-            if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+            if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
             {
-                NavigationManager.NavigateTo("/");
+                NavigationManager.NavigateTo("/countries");
             }
             else
             {
-                var messageError = await responseHttp.GetErrorMessageAsync();
-                await SweetAlertService.FireAsync(Localizer["Error"], Localizer[messageError!], SweetAlertIcon.Error);
+                var message = await responseHttp.GetErrorMessageAsync();
+                Snackbar.Add(Localizer[message!], Severity.Error);
             }
             return;
         }
-
-        await LoadAsync();
-        var toast = SweetAlertService.Mixin(new SweetAlertOptions
-        {
-            Toast = true,
-            Position = SweetAlertPosition.BottomEnd,
-            ShowConfirmButton = true,
-            Timer = 3000,
-            ConfirmButtonText = Localizer["Yes"]
-        });
-        toast.FireAsync(icon: SweetAlertIcon.Success, message: Localizer["RecordDeletedOk"]);
+        await LoadTotalRecordsAsync();
+        await table.ReloadServerData();
+        Snackbar.Add(Localizer["RecordDeletedOk"], Severity.Success);
     }
 }
