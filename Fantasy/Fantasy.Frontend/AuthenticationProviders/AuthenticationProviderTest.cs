5b7ae394938e8871cb6e94f9833c4f966c823679
Juan Carlos Zuluaga Cardona <jzuluaga55@gmail.com>
2024-09-20 19:54:22 -0500

    Simplify list

--- Fantasy/Fantasy.Frontend/AuthenticationProviders/AuthenticationProviderTest.cs
+++ Fantasy/Fantasy.Frontend/AuthenticationProviders/AuthenticationProviderTest.cs
@@ -11,13 +11,13 @@ public class AuthenticationProviderTest : AuthenticationStateProvider
 
         var anonimous = new ClaimsIdentity();
         var user = new ClaimsIdentity(authenticationType: "test");
-        var admin = new ClaimsIdentity(new List<Claim>
-        {
+        var admin = new ClaimsIdentity(
+        [
             new Claim("FirstName", "Juan"),
             new Claim("LastName", "Zulu"),
             new Claim(ClaimTypes.Name, "zulu@yopmail.com"),
             new Claim(ClaimTypes.Role, "Admin")
-        },
+        ],
         authenticationType: "test");
 
         return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(admin)));
